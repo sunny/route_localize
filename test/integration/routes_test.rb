39369a5a6a7d226e2c9113a855afb8cc100cdb61
Sunny Ripert <sunny@sunfox.org>
2013-09-01 13:09:01 +0200

    Test path names and url_for

--- test/integration/routes_test.rb
+++ test/integration/routes_test.rb
@@ -1,7 +1,22 @@
+# encoding: utf-8
 require 'test_helper'
 
 class RoutesTest < ActionDispatch::IntegrationTest
   fixtures :all
+
+  # Mock object that acts like a routeable ActiveRecord model
+  class FakeTree
+    attr_reader :to_param
+    def initialize(to_param)
+      @to_param = to_param
+    end
+    def self.model_name
+      self
+    end
+    def self.singular_route_key
+      "tree"
+    end
+  end
 
   include ActionDispatch::Routing::UrlFor
   include Rails.application.routes.url_helpers
@@ -9,21 +24,29 @@ class RoutesTest < ActionDispatch::IntegrationTest
   test "paths" do
     # en
     assert_equal "/home", home_en_path
-    assert_equal "/trees/new", new_tree_en_path
+    assert_equal "/trees/plant", new_tree_en_path
+    assert_equal "/trees/42", tree_en_path(42)
+    assert_equal "/trees/42/edit", edit_tree_en_path(42)
 
     # fr
     assert_equal "/accueil", home_fr_path
-    assert_equal "/arbres/nouveau", new_tree_fr_path
+    assert_equal "/arbres/planter", new_tree_fr_path
+    assert_equal "/arbres/42", tree_fr_path(42)
+    assert_equal "/arbres/42/%C3%A9diter", edit_tree_fr_path(42)
   end
 
   test "generic paths depend on locale" do
     I18n.locale = :en
     assert_equal "/home", home_path
-    assert_equal "/trees/new", new_tree_path
+    assert_equal "/trees/plant", new_tree_path
+    assert_equal "/trees/42", tree_path(42)
+    assert_equal "http://en.example.com/trees/42", url_for(FakeTree.new(42))
 
     I18n.locale = :fr
     assert_equal "/accueil", home_path
-    assert_equal "/arbres/nouveau", new_tree_path
+    assert_equal "/arbres/planter", new_tree_path
+    assert_equal "/arbres/42", tree_path(42)
+    assert_equal "http://fr.example.com/arbres/42", url_for(FakeTree.new(42))
   end
 
   test "generates paths depending on subdomain" do
