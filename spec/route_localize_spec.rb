ea559239a4edcefaead0cbdc87a9bcc5e7dc3830
Sunny Ripert <sunny@sunfox.org>
2014-10-05 12:50:39 +0200

    localize-by-path WIP

--- spec/route_localize_spec.rb
+++ spec/route_localize_spec.rb
@@ -54,7 +54,7 @@ describe RouteLocalize do
   describe '.define_locale_helpers' do
     let(:helper) { double(:helper, define_method: nil) }
 
-    it "defines methods that call the full path in the current locale" do
+    before do
       # Spy on the generated methods beeing called
       RSpec::Mocks.configuration.verify_partial_doubles = false
       allow(RouteLocalize).to receive(:bang_fr_url)
@@ -63,23 +63,38 @@ describe RouteLocalize do
       allow(RouteLocalize).to receive(:bang_en_path)
 
       # Store the methods defined
-      bang_path = bang_url = nil
-      expect(helper).to receive(:define_method).with("bang_path") { |&arg| bang_path = arg }
-      expect(helper).to receive(:define_method).with("bang_url") { |&arg| bang_url = arg }
+      @bang_path = @bang_url = nil
+      expect(helper).to receive(:define_method).with("bang_path") { |&m| @bang_path = m }
+      expect(helper).to receive(:define_method).with("bang_url") { |&m| @bang_url = m }
+    end
+
+    it "defines path in each locale" do
+      RouteLocalize.define_locale_helpers("bang", helper)
+
+      I18n.locale = :fr
+      @bang_path.call
+      expect(RouteLocalize).to have_received(:bang_fr_path).once
+
+      I18n.locale = :en
+      @bang_path.call
+      expect(RouteLocalize).to have_received(:bang_en_path).once
+    end
+
+    it "defines methods that call the path and url of the current locale" do
       RouteLocalize.define_locale_helpers("bang", helper)
 
       # When in french, call the french methods
       I18n.locale = :fr
-      bang_path.call
+      @bang_path.call
       expect(RouteLocalize).to have_received(:bang_fr_path).once
-      bang_url.call
+      @bang_url.call
       expect(RouteLocalize).to have_received(:bang_fr_url).once
 
       # When in english, call the english methods
       I18n.locale = :en
-      bang_path.call
+      @bang_path.call
       expect(RouteLocalize).to have_received(:bang_en_path).once
-      bang_url.call
+      @bang_url.call
       expect(RouteLocalize).to have_received(:bang_en_url).once
     end
   end
